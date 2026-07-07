03bc0b8bd8bec85389a7ce38ab1367420576d29f
Nguyen Hieu Hoc <hieuhoc1@ASUS.localdomain>
2026-07-07 16:21:11 +0000

    Day 1

---
+++ ttcn.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    cout << "Name: Nguyen Hieu Hoc" << endl;
+    cout << "Language: C++" << endl;
+    cout << "Goal: 5G Radio Software Intern" << endl;
+    return 0;
+}
